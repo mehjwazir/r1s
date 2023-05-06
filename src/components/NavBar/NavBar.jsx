056e72c850e7b5418327e2216cfb36c503096013
mehjwazir <mehjwazir@gmail.com>
2023-05-06 00:44:19 -0700

    fixed the screensize issue with useEffect that was creating a problem going back to desktop nav from hamburger menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,8 @@ const NavBar = () => {
 
 	useEffect(() => {
 		const handleResize = () => {
-			setIsMobile(window.innerWidth <= 1280);
+			setIsMobile(window.innerWidth <= 1080);
+			setIsSidebarOpen(false); // close sidebar on resize
 		};
 
 		window.addEventListener("resize", handleResize);
@@ -21,6 +22,7 @@ const NavBar = () => {
 		// Clean up event listener on unmount
 		return () => window.removeEventListener("resize", handleResize);
 	}, []);
+
 
 	useEffect(() => {
 		const handleClickOutside = (event) => {
@@ -36,7 +38,7 @@ const NavBar = () => {
 	}, [navRef]);
 
 	const handleSidebarToggle = () => {
-		setIsSidebarOpen(!isSidebarOpen);
+		setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen);
 	};
 
 	return (
@@ -49,8 +51,8 @@ const NavBar = () => {
 						<Link to="/">home</Link>
 					</li>
 					{/* <li>
-              <Link to="/shop">shop</Link>
-            </li> */}
+            <Link to="/shop">shop</Link>
+          </li> */}
 					<li>
 						<Link to="/about">about</Link>
 					</li>
@@ -62,13 +64,15 @@ const NavBar = () => {
 					</li>
 				</ul>
 				{isMobile && (
-
-					<div className="hamburger-menu" onClick={handleSidebarToggle} aria-label="Toggle navigation menu">
-				<div className="hamburger-line"></div>
-				<div className="hamburger-line"></div>
-				<div className="hamburger-line"></div>
-			</div>
-				
+					<div
+						className="hamburger-menu"
+						onClick={handleSidebarToggle}
+						aria-label="Toggle navigation menu"
+					>
+						<div className="hamburger-line"></div>
+						<div className="hamburger-line"></div>
+						<div className="hamburger-line"></div>
+					</div>
 				)}
 				{isSidebarOpen && <SideBar />}
 			</nav>
@@ -82,68 +86,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
-
-
-
-
-
-// Mobile menu test code 
-// import './NavBar.css'
-// import { Link } from 'react-router-dom';
-// import { useState } from 'react'
-
-
-
-
-
-// const NavBar = () => {
-
-// 	const [show, setShow] = useState(false)
-
-// 	function showSwitch() {
-// 		return setShow(!show)
-// 	}
-
-
-
-// 	return (
-// 		<header>
-// 			<nav>
-// 				<ul className={show ? 'nav-links active' : 'nav-links'}>
-// 					<li>
-// 						<Link onClick={() => showSwitch()} show={show} to="/">home</Link>
-// 					</li>
-// 					{/* <li>
-// 						<Link to="/shop">shop</Link>
-// 					</li> */}
-// 					<li>
-// 						<Link onClick={() => showSwitch()} show={show} to="/about">about</Link>
-// 					</li>
-// 					<li>
-// 						<Link onClick={() => showSwitch()} show={show} to="/studio"> the studio</Link>
-// 					</li>
-// 					<li>
-// 						<Link onClick={() => showSwitch()} show={show} to="/inquire">inquire</Link>
-// 					</li>
-// 					<div onClick={() => showSwitch()} className={show ? 'bars-button active' : 'bars-button '}>
-// 						<span></span>
-// 						<span></span>
-// 						<span></span>
-// 					</div>
-// 				</ul>
-// 			</nav>
-// 			<div className="logo-div">
-// 				<Link to="/" className="logo">
-// 					<h1 className="logo">r1s</h1>
-// 				</Link>
-// 			</div>
-// 		</header>
-// 	);
-// };
-
-
-
-
-
-// export default NavBar;
