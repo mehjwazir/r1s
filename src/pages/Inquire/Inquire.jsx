a6266780444f5e3b2b9c551981761fb017e40638
mehjwazir <mehjwazir@gmail.com>
2023-05-06 20:41:19 -0700

    added inquire form code with useRef template id service id and public key  with emailJs and npm emailjs/browser

--- src/pages/Inquire/Inquire.jsx
+++ src/pages/Inquire/Inquire.jsx
@@ -1,15 +1,40 @@
 import './Inquire.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 
-function Inquire() {
+const Inquire = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs.sendForm('service_1fog7jr', 'template_gnv522h', form.current, '38R-GxB-JxYZ7fTz5')
+			.then((result) => {
+				console.log(result.text);
+				console.log('message sent');
+				e.target.reset();
+			}, (error) => {
+				console.log(error.text);
+			});
+	};
 
 	return (
 		<main className='inquire'>
 			<h1>Inquire</h1>
+			<form ref={form} onSubmit={sendEmail}>
+				<label>Name</label>
+				<input type="text" name="user_name" />
+				<label>Email</label>
+				<input type="email" name="user_email" />
+				<label>Message</label>
+				<textarea name="message" />
+				<input type="submit" value="Send" />
+			</form>
 			<div className='inquire-div'>
-				<ul>
 
+				<ul>
 					<li>Brand Identity</li>
 					<li>Custom Logotype</li>
 					<li>Website Design & Development</li>
