934f25efcdc30113a056848f51f741ee14e6c726
mehjwazir <mehjwazir@gmail.com>
2023-05-30 01:58:40 -0700

    added two images for the first two projects in studio.jsx img8 img7 bottom row

--- src/pages/Studio/Studio.jsx
+++ src/pages/Studio/Studio.jsx
@@ -1,6 +1,8 @@
 import './Studio.css'
 import { Link} from 'react-router-dom';
 import Img1 from './files/IP.png'
+import Img8 from './files/ID.png'
+import Img7 from './files/Aura.png'
 
 
 
@@ -19,8 +21,8 @@ function Studio() {
 				<div>
 					<Link to='/ip'><img src={Img1} alt="ip" className="image" /></Link>
 					<Link to='/ip'><img src={Img1} alt="ip" className="image" /></Link>
-					<Link to='/ar'><img src={Img1} alt="ar" className="image" /></Link>
-					<Link to='/id'><img src={Img1} alt="id" className="image" /></Link>
+					<Link to='/ar'><img src={Img7} alt="ar" className="image" /></Link>
+					<Link to='/id'><img src={Img8} alt="id" className="image" /></Link>
 				</div>
 			</div>
 		</main>
