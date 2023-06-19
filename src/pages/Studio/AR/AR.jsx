7e02a6229ce9a5f38922b04af655baa1fb4bd992
mehjwazir <mehjwazir@gmail.com>
2023-06-19 00:23:01 -0700

    added ar and studo pics and files

--- src/pages/Studio/AR/AR.jsx
+++ src/pages/Studio/AR/AR.jsx
@@ -1,10 +1,18 @@
 import "./AR.css"
+import Img1 from "./files/img1.png"
+import Img2 from "./files/img2.png"
 
 
 export default function AR() {
 	return (
 		<main className="ar-main">
-			<h1>AURA</h1>
+			<div className="img1-ar-div">
+				<img src={Img1} alt="" className="img1-ar" />
+			</div>
+			<div className="img2-ar-div">
+				<img src={Img2} alt="" className="img2-ar" />
+			</div>
+	
 
 		</main>
 	)
