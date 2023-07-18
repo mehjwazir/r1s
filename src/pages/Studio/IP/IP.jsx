bb2d09604b7ae05167dbfb911cd34003fb590dc2
mehjwazir <mehjwazir@gmail.com>
2023-07-17 23:36:45 -0700

    added IP imgs 1-4 and styled in css

--- src/pages/Studio/IP/IP.jsx
+++ src/pages/Studio/IP/IP.jsx
@@ -1,4 +1,9 @@
 import './IP.css'
+import Img1 from "./files/img1.jpg"
+import Img2 from "./files/img2.jpg"
+import Img3 from "./files/img3.jpg"
+import Img4 from "./files/img4.jpg"
+
 
 
 
@@ -7,7 +12,17 @@ function IP() {
 
 	return (
 		<main className='ip'>
-			<h1>IP</h1>
+			<div className='img1-ip-div'>
+				<img src={Img2} alt="img2"className='img2-ip' />
+				<img src={Img1} alt="img1" className='img1-ip' />
+			</div>
+			<div>
+				<img src={Img3} alt="img3" className='img3-ip' />
+			</div>
+			<div>
+				
+				<img src={Img4} alt="img4" className='img4-ip' />
+			</div>
 		
 		</main>
 	)
