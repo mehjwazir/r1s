2b35b1e0fbaf9774b7fc8d70ad93440a4cc50ae0
mehjwazir <mehjwazir@gmail.com>
2023-07-26 00:48:58 -0700

    added Img2 in CR folder files and in CR.jsx

--- src/pages/Studio/CR/CR.jsx
+++ src/pages/Studio/CR/CR.jsx
@@ -1,5 +1,6 @@
 import './CR.css'
 import Img1 from './files/Img1.png'
+import Img2 from './files/Img2.png'
 
 
 
@@ -8,7 +9,10 @@ function CR() {
 		<main className='cr'>
 			<div>
 				<img src={Img1} alt="cors" className='img1-cr' />
-		</div>
+			</div>
+			<div>
+				<img src={Img2} alt="cors" className='img1-cr' />
+			</div>
 		</main>
 	)
 }
