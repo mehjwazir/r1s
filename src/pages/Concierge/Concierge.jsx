9d9be1607990cda93a4ed79d76e59a2eccffc174
mehjwazir <mehjwazir@gmail.com>
2023-08-05 07:43:45 -0700

    fixed mobile ui in Concierge.css and added different class name in Concierge.jsx

--- src/pages/Concierge/Concierge.jsx
+++ src/pages/Concierge/Concierge.jsx
@@ -7,9 +7,10 @@ function Concierge() {
 	return (
 		<main className='concierge'>
 			<h1 className='concierge-h1'>FAQ</h1>
+			<h2 className='concierge-h2'>Terms</h2>
 			<div>
-				<h2 className='concierge-h2'>Terms</h2>
-				<p className='privacy-p'>PRIVACY POLICY
+			
+				<p className='privacy-policy privacy-p'>PRIVACY POLICY
 					<br />
 
 					This privacy policy sets out how ROHE 1 STUDIO uses and protects any information that you give ROHE 1 STUDIO when you use this website. ROHE 1 STUDIO is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement. ROHE 1 STUDIO may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
