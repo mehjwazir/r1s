7a33f6889a9ffb0e164522f435f9e3dc19fecd6c
mehjwazir <mehjwazir@gmail.com>
2023-08-06 19:38:14 -0700

    removed imgs 1-6 9-12 in Type files folder and added new ones. Also adjusted code accordingly in Type.jsx

--- src/pages/Studio/Type/Type.jsx
+++ src/pages/Studio/Type/Type.jsx
@@ -7,7 +7,9 @@ import Img5 from './files/5.png'
 import Img6 from './files/6.png'
 import Img7 from './files/7.png'
 import Img8 from './files/8.png'
+import Img9 from './files/9.png'
 import Img10 from './files/10.png'
+import Img11 from './files/11.png'
 import Img12 from './files/12.png'
 
 
@@ -24,9 +26,9 @@ function Type() {
 				<img src={Img6} alt="6" className='type1' />
 				<img src={Img7} alt="7" className='type1' />
 				<img src={Img8} alt="8" className='type1' />
-		
+				<img src={Img9} alt="9" className='type1' />
 				<img src={Img10} alt="10" className='type1' />
-				
+				<img src={Img11} alt="11" className='type1' />
 				<img src={Img12} alt="12" className='type1' />
 			</div>
 		</main>
