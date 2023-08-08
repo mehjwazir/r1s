c4fa1c6494d95547ff9e39aa61b3e35cd3176808
mehjwazir <mehjwazir@gmail.com>
2023-08-07 17:55:45 -0700

    testing another version of the gallery with description

--- src/pages/Studio/Studio.jsx
+++ src/pages/Studio/Studio.jsx
@@ -1,5 +1,5 @@
 import './Studio.css'
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Img1 from './files/Type.png'
 import Img2 from './files/R1S.png'
 import Img3 from './files/CR.jpg'
@@ -9,28 +9,42 @@ import Img6 from './files/IPP.png'
 import Img7 from './files/Aura.png'
 import Img8 from './files/ID.png'
 
-
-
-
 function Studio() {
-
 	return (
-		
 		<main className='studio'>
 			<div className="gallery">
-				<div>
-					{/* Add description to photos and fix the two that need to be color. */}
-					<Link to='/type'><img src={Img1} alt="type" className="image filter" /></Link>
-					<Link to='/r1s'><img src={Img2} alt="r1s" className="image filter" /></Link>
-					<Link to='/cr'><img src={Img3} alt="cr" className="image filter" /></Link>
-					<Link to='/hf'><img src={Img4} alt="hf" className="image filter" /></Link>
-				</div>
-				<div>
-					<Link to='/hasu'><img src={Img5} alt="hasu" className="image filter" /></Link>
-					<Link to='/ip'><img src={Img6} alt="ip" className="image filter" /></Link>
-					<Link to='/aura'><img src={Img7} alt="ar" className="image filter" /></Link>
-					<Link to='/id'><img src={Img8} alt="id" className="image filter" /></Link>
-				</div>
+				<Link to='/type' className="image-container">
+					<img src={Img1} alt="type" className="image filter" />
+					<p className="image-description">Description for Image 1</p>
+				</Link>
+				<Link to='/r1s' className="image-container">
+					<img src={Img2} alt="r1s" className="image filter" />
+					<p className="image-description">Description for Image 2</p>
+				</Link>
+				<Link to='/cr' className="image-container">
+					<img src={Img3} alt="cr" className="image filter" />
+					<p className="image-description">Description for Image 3</p>
+				</Link>
+				<Link to='/hf' className="image-container">
+					<img src={Img4} alt="hf" className="image filter" />
+					<p className="image-description">Description for Image 4</p>
+				</Link>
+				<Link to='/hasu' className="image-container">
+					<img src={Img5} alt="hasu" className="image filter" />
+					<p className="image-description">Description for Image 5</p>
+				</Link>
+				<Link to='/ip' className="image-container">
+					<img src={Img6} alt="ip" className="image filter" />
+					<p className="image-description">Description for Image 6</p>
+				</Link>
+				<Link to='/aura' className="image-container">
+					<img src={Img7} alt="aura" className="image filter" />
+					<p className="image-description">Description for Image 7</p>
+				</Link>
+				<Link to='/id' className="image-container ">
+					<img src={Img8} alt="id" className="image filter" />
+					<p className="image-description">Description for Image 8</p>
+				</Link>
 			</div>
 		</main>
 	)
