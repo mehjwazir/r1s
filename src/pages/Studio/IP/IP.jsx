46be701b66cd2ea90e630f3143159bbe72899530
mehjwazir <mehjwazir@gmail.com>
2023-08-10 18:39:01 -0700

    Made paragraph in IP.jsx smaller, added border box styling in ID.css, added filter class and filter hover in Studio.css to be none when changed to a smaller screen size

--- src/pages/Studio/IP/IP.jsx
+++ src/pages/Studio/IP/IP.jsx
@@ -26,7 +26,7 @@ function IP() {
 			<div className="row row2">
 				<div className='col col2'>
 					<p>
-						Inner Peace serves as a tranquil haven, inviting you to embark on a harmonious exploration of the interconnected realms of mind, body, soul, and nature. With a wealth of wisdom and inspiration, we offer an array of carefully curated resources that empower you to cultivate a deeper understanding of yourself and the world around you. Delve into mindfulness practices that soothe the mind, engage in revitalizing activities that rejuvenate the body, embrace spiritual insights that nourish the soul, and revel in the awe-inspiring beauty of nature's embrace. This digital oasis is your gateway to finding serenity in the midst of life's bustling currents, a virtual refuge where you can harmonize your being and draw endless inspiration for a more balanced and fulfilling existence.
+						Inner Peace serves as a tranquil haven, inviting you to embark on a harmonious exploration of the interconnected realms of mind, body, soul, and nature. With a wealth of wisdom and inspiration, we offer an array of carefully curated resources that empower you to cultivate a deeper understanding of yourself and the world around you. 
 					</p>
 				</div>
 				
